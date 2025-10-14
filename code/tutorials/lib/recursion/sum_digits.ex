2e415311d449578e482cc4c941438ad33385a5cf
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2025-10-14 17:55:58 +0300

    recursion

--- code/tutorials/lib/recursion/sum_digits.ex
+++ code/tutorials/lib/recursion/sum_digits.ex
@@ -3,6 +3,13 @@ defmodule Tutorials.Recursion.SumDigits do
   def up_to(0), do: 0
 
   def up_to(num) do
-    num + up_to(num - 1)
+    up_to_tail_recursive(num)
+  end
+
+  def up_to_tail_recursive(num, acc \\ 0)
+  def up_to_tail_recursive(0, acc), do: acc
+
+  def up_to_tail_recursive(num, acc) do
+    up_to_tail_recursive(num - 1, acc + num)
   end
 end
