eefcc5c3b88e09acc25e3ac1d4fbd95e3c39da57
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2025-10-15 10:39:18 +0300

    function definition update

--- code/tutorials/lib/recursion/print_digits.ex
+++ code/tutorials/lib/recursion/print_digits.ex
@@ -1,16 +1,16 @@
 defmodule Tutorials.Recursion.PrintDigits do
   # Base case
-  def up_to(0), do: :ok
+  def upto(0), do: :ok
 
   #   # Tail Recursion (printing executed in ascending stage)
-  #   def up_to(num) do
+  #   def upto(num) do
   #     IO.puts(num)
-  #     up_to(num - 1)
+  #     upto(num - 1)
   #   end
 
   # Head Recursion (printing executed in return stage)
-  def up_to(num) do
-    up_to(num - 1)
+  def upto(num) do
+    upto(num - 1)
     IO.puts(num)
   end
 end
--- code/tutorials/lib/recursion/sum_digits.ex
+++ code/tutorials/lib/recursion/sum_digits.ex
@@ -1,15 +1,15 @@
 defmodule Tutorials.Recursion.SumDigits do
   # base condition
-  def up_to(0), do: 0
+  def upto(0), do: 0
 
-  def up_to(num) do
-    up_to_tail_recursive(num)
+  def upto(num) do
+    upto_tail_recursive(num)
   end
 
-  def up_to_tail_recursive(num, acc \\ 0)
-  def up_to_tail_recursive(0, acc), do: acc
+  def upto_tail_recursive(num, acc \\ 0)
+  def upto_tail_recursive(0, acc), do: acc
 
-  def up_to_tail_recursive(num, acc) do
-    up_to_tail_recursive(num - 1, acc + num)
+  def upto_tail_recursive(num, acc) do
+    upto_tail_recursive(num - 1, acc + num)
   end
 end
