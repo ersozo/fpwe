b47a5967671cb242da4e4747b0dc0b5a24d8a4c9
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2025-10-17 16:52:14 +0300

    sum, reverse, map

--- code/tutorials/lib/lists/lists.ex
+++ code/tutorials/lib/lists/lists.ex
@@ -8,6 +8,9 @@ defmodule Tutorials.Lists do
   @doc """
   Returns the sum of numbers in a list.
   """
+
+  #   ------------------- Sum ---------------------------
+
   @spec sum(list(number())) :: number()
   def sum(nums), do: sum_tail_rec(nums)
 
@@ -19,4 +22,18 @@ defmodule Tutorials.Lists do
   def sum_tail_rec(nums, acc \\ 0)
   def sum_tail_rec([], acc), do: acc
   def sum_tail_rec([h | t], acc), do: sum_tail_rec(t, h + acc)
+
+  #   ------------------- Reverse ----------------------
+
+  #   @spec reverse(list(any()), list(any())):: list(any())
+  @spec reverse([any()], [any()]) :: [any()]
+  def reverse(elements, acc \\ [])
+  def reverse([], acc), do: acc
+  def reverse([h | t], acc), do: reverse(t, [h | acc])
+
+  #   ------------------- Map --------------------------
+  @spec map([any()], (any() -> any()), [any()]) :: [any()]
+  def map(elements, func, acc \\ [])
+  def map([], _, acc), do: reverse(acc)
+  def map([h | t], func, acc), do: map(t, func, [func.(h) | acc])
 end
